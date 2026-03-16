417a20949a90c5ea9108a65cbcab1d626fffe88c
dailin <2687008066@qq.com>
2026-03-16 22:34:43 +0800

    blog auto-commit: 2026-03-16 22:34:43

---
+++ script/sync-blog.sh
@@ -0,0 +1,19 @@
+cd ..
+
+TIME=$(date +"%Y-%m-%d %H:%M:%S")
+echo "[$TIME] 开始与云端同步..."
+
+git pull --rebase origin "$BRANCH"
+
+if[ $? -ne 0 ]; then
+    echo "[$TIME] ❌ 拉取云端代码失败（可能存在冲突），停止同步，请手动处理。"
+    exit 1
+fi
+
+git push origin "$BRANCH"
+
+if [ $? -eq 0 ]; then
+    echo "[$TIME] ✅ 成功拉取并推送到云端。"
+else
+    echo "[$TIME] ❌ 推送失败，请检查网络或 SSH 权限。"
+fi
