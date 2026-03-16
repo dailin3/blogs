b27d26de9e851cc71aa9cb27151924c3c371b18e
dailin <2687008066@qq.com>
2026-03-16 22:41:35 +0800

    blog auto-commit: 2026-03-16 22:41:35

--- script/sync-blog.sh
+++ script/sync-blog.sh
@@ -3,14 +3,14 @@ cd ..
 TIME=$(date +"%Y-%m-%d %H:%M:%S")
 echo "[$TIME] 开始与云端同步..."
 
-git pull --rebase origin "$BRANCH"
+git pull --rebase origin main
 
 if[ $? -ne 0 ]; then
     echo "[$TIME] ❌ 拉取云端代码失败（可能存在冲突），停止同步，请手动处理。"
     exit 1
 fi
 
-git push origin "$BRANCH"
+git push origin main
 
 if [ $? -eq 0 ]; then
     echo "[$TIME] ✅ 成功拉取并推送到云端。"
